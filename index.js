83318efc7b2f66cc785cb13f9c1a3dc0128ad3fc
Collins Orego <181986390+Collins101-dev@users.noreply.github.com>
2025-05-04 02:32:56 +0300

    Fixed the displaying to console

--- index.js
+++ index.js
@@ -1,71 +1,63 @@
-function getComputerChoice(){
+function getComputerChoice() {
     let choice = Math.random();
 
     if (choice < 0.3) {
-        let answer = "rock";
-        return answer;
-    } else if (choice > 0.3 && choice < 0.6) {
-        let answer = "paper";
-        return answer;
+        return "rock";
+    } else if (choice < 0.6) {
+        return "paper";
     } else {
-        let answer = "scissors";
-        return answer;
+        return "scissors";
     }
 }
 
-function getHumanChoice(){
+function getHumanChoice() {
     let humanChoice = prompt("Enter your choice (rock, paper, or scissors):");
-    humanChoice = humanChoice.toLowerCase();
-    return humanChoice;
+    return humanChoice.toLowerCase();
 }
 
-humanScore = 0;
-computerScore = 0;
+let humanScore = 0;
+let computerScore = 0;
 
-let humanSelection = getHumanChoice();
-let computerSelection = getComputerChoice();
-
-function playRound(humanChoice, computerChoice){
-    humanChoice = humanSelection;
-    computerChoice = computerSelection;
-
-    if (humanChoice == "rock" &&  computerChoice == "scissors"){
-        console.log("You win. Rock beats sciccors.");
-        humanScore += 1;
-    } else if (humanChoice == "paper" &&  computerChoice == "rock") {
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === "rock" && computerChoice === "scissors") {
+        console.log("You win. Rock beats scissors.");
+        humanScore++;
+    } else if (humanChoice === "paper" && computerChoice === "rock") {
         console.log("You win. Paper beats rock.");
-        humanScore += 1;
-    } else if (humanChoice == "scissors" &&  computerChoice == "paper") {
+        humanScore++;
+    } else if (humanChoice === "scissors" && computerChoice === "paper") {
         console.log("You win. Scissors beats paper.");
-        humanScore += 1;
-    } else if (computerChoice == "rock" &&  humanChoice == "scissors"){
-        console.log("You lose. Rock beats sciccors.");
-        computerScore += 1;
-    } else if (computerChoice == "paper" &&  humanChoice == "rock"){
+        humanScore++;
+    } else if (computerChoice === "rock" && humanChoice === "scissors") {
+        console.log("You lose. Rock beats scissors.");
+        computerScore++;
+    } else if (computerChoice === "paper" && humanChoice === "rock") {
         console.log("You lose. Paper beats rock.");
-        computerScore += 1;
-    } else if (computerChoice == "scissors" &&  humanChoice == "paper"){
+        computerScore++;
+    } else if (computerChoice === "scissors" && humanChoice === "paper") {
         console.log("You lose. Scissors beats paper.");
-        computerScore += 1;
-    } else if (humanChoice == computerChoice){
-        console.log(`A tie. ${humanChoice} is same as ${computerChoice}.`)
+        computerScore++;
+    } else if (humanChoice === computerChoice) {
+        console.log(`A tie. ${humanChoice} is same as ${computerChoice}.`);
+    } else {
+        console.log("Invalid input. Please choose rock, paper, or scissors.");
     }
-
 }
 
-function playGame(){
-    for (let i = 1; i < 5; i++){
+function playGame() {
+    for (let i = 0; i < 5; i++) {
         let humanSelection = getHumanChoice();
         let computerSelection = getComputerChoice();
         playRound(humanSelection, computerSelection);
     }
 
-    if (humanScore > computerScore){
-        console.log(`Congrats! You win with a score of ${humanScore}.`)
-
-    }else if (computerScore > humanScore) {
-        console.log(`Sorry. You lose to computer's score of ${computerScore}`)
+    if (humanScore > computerScore) {
+        console.log(`Congrats! You win with a score of ${humanScore}.`);
+    } else if (computerScore > humanScore) {
+        console.log(`Sorry. You lose to the computer's score of ${computerScore}.`);
+    } else {
+        console.log("It's a tie game!");
     }
 }
 
-playGame()
+playGame();
